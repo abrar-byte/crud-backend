f45c8421928468119d1c7b19323e636d0d89e22d
abrar-byte <usamahhafidz99@gmail.com>
2022-07-27 20:44:27 +0700

    ready

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ import cors from "cors";
 import UserRoute from "./routes/UserRoute.js";
 
 const app = express();
-const port = Process.env.PORT || 5000
+const port = process.env.PORT || 5000
 app.use(cors());
 app.use(express.json());
 app.use(UserRoute);
 app.get("/",(req,res)=>res.send('congrats'))
-app.listen(port, ()=> console.log('Server up and running...'));
+app.listen(process.env.PORT || 5000, ()=> console.log('Server up and running...'));
